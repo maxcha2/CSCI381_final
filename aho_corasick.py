93fd7d073520689fd517f3892288a4c925a05815
Max Cha <mcha@oberlin.edu>
2023-05-12 03:53:45 -0400

    finished

--- aho_corasick.py
+++ aho_corasick.py
@@ -1,5 +1,7 @@
 from collections import deque
 import sys
+import time
+import matplotlib.pyplot as plt
 
 class AhoCorasick:
     def __init__(self, keywords):
@@ -14,7 +16,7 @@ class AhoCorasick:
                     current_node['next'][char] = {'next': {}, 'fail': None, 'output': set()}
                 current_node = current_node['next'][char]
             current_node['output'].add(keyword)
-        
+        #print(self.root)
         # Compute the fail function for each node in the trie
         self.fail()
         
@@ -44,31 +46,51 @@ class AhoCorasick:
                 continue
             current_node = current_node['next'][char]
             results.extend(current_node['output'])
-        return results, i
+        return results
     
 def main():
-    args = sys.argv
-    file1 = args[1]
-    file2 = args[2]
+    #args = sys.argv
+    apoe_file = "apoe.txt"
+    dna_one = "GCA_000002125.2_HuRef_genomic.fna"
+    dna_two = "GCA_000004845.2_YH_2.0_genomic.fna"
+    dna_three = "GCF_000306695.2_CHM1_1.1_genomic.fna"
+    dna_four = "GCA_009914755.4_T2T-CHM13v2.0_genomic.fna"
+    dna_five = "GCF_000001405.40_GRCh38.p14_genomic.fna"
+    list_of_dna = [dna_one, dna_two, dna_three, dna_four, dna_five]
 
-    f = open(file1)
-    sample = open(file2)
-
+    f = open(apoe_file)
     apoe = ""
-    human_genome = ""
 
     for char in f.read():
         if char != "\n":
             apoe += char
-
-    for char in sample.read():
-        if char != "\n":
-            human_genome += char
     
-    keywords = [apoe]
-    ac = AhoCorasick(keywords)
-    results = ac.search(human_genome)
-    print(results)
+    y = []
 
+    for dna in list_of_dna:
+        human_genome = ""
+        pre_process_start = time.time()
+        with open(dna, 'r') as file:
+            human_genome = file.read().replace('\n', '')
+        pre_process_end = time.time()
+        
+        keywords = [apoe]
+
+        algorithm_start = time.time()
+        ac = AhoCorasick(keywords)
+        result = ac.search(human_genome)
+        algorithm_end = time.time()
+            
+        print(dna, "========================")
+        print("Preprocessing data runtime: ", pre_process_end - pre_process_start)
+        print("Algorithm runtime: ", algorithm_end - algorithm_start)
+ 
+    y = ['Contains', 'Does not']
+    list_of_d = ["1", "2", "3", "4", "5"]
+    plt.xlabel('Contains/Does not contain APOE4', fontsize=14)
+    plt.ylabel("Number of human DNA tested", fontsize=14)
+    plt.bar(list_of_d, y)
+    plt.show()
+    
 if __name__ == "__main__":
     main()
